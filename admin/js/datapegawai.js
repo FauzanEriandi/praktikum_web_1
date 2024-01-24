ca85502d7bf1d87e5feaa3850c48a1cd54540e2d
Fauzan Eriandi <fauzaneriandi02@gmail.com>
2024-01-24 15:09:04 +0700

    menambahkan folder project 2.0

---
+++ admin/js/datapegawai.js
@@ -0,0 +1,9 @@
+function editRow() {
+    // Logika untuk mengedit baris
+    alert("Fungsi edit belum diimplementasikan");
+}
+
+function deleteRow() {
+    // Logika untuk menghapus baris
+    alert("Fungsi hapus belum diimplementasikan");
+}
